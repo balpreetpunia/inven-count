0411a56ec69f500d39a3b349d3731887db77b7d0
balpreetpunia <mebalpreetpunia@gmail.com>
2018-02-22 14:23:41 -0500

    Added add.php to add new models to the database.

---
+++ add.php
@@ -0,0 +1,29 @@
+<?php
+
+    $model = isset($_GET['model']) ? $_GET['model'] : '';
+
+    if($model != ''){
+
+        require 'shared/connect.php';
+
+        $sql = "select model from models where model = '$model'";
+        $sth = $dbh->prepare($sql);
+        $sth->execute();
+        $count = $sth->rowCount();
+
+        if ($count > 0){
+
+        }
+        else{
+            $sql = "INSERT INTO models VALUES ('$model',0)";
+            $dbh->exec($sql);
+        }
+
+        $dbh = null;
+
+    }
+
+    header('Location: /count/');
+
+
+?>
--- index.php
+++ index.php
@@ -64,7 +64,14 @@
     </div>
 </div>
 <hr>
-<h3 class="text-center"><?php if ($error==0){echo"$model Added";} elseif($error ==1){echo "Model not found";}?></h3>
+<h3 class="text-center">
+    <?php
+        if ($error==0){echo"$model added";}
+        elseif($error ==1){
+            echo "Model not found <a href='add.php?model=$model'>Add $model to Database</a>";
+        }
+    ?>
+</h3>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
