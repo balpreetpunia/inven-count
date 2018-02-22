ae057b114fed1f2477e52fa6368b943af7767240
balpreetpunia <mebalpreetpunia@gmail.com>
2018-02-22 14:30:12 -0500

    Added add.php to add new models to the database.

--- shared/connect.php
+++ shared/connect.php
@@ -5,5 +5,5 @@
         $dbh = new PDO("mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=acsm_03a9a5b692a77f1","b81a903bbbb0b7","606b3f2e");
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e){
-    echo 'Connection failed: ' . $e->getMessage();
+    echo 'Database Connection failed: ' . $e->getMessage();
     }
