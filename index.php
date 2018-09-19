be7327c60612edc6006d62ee49321eb41bf7e0ea
balpreetpunia <mebalpreetpunia@gmail.com>
2018-09-18 22:05:42 -0400

    Added icons.

--- index.php
+++ index.php
@@ -49,14 +49,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-aNUYGqSUL9wG/vP7+cWZ5QOM4gsQou3sBfWRr/8S3R1Lv0rysEmnwsRKMbhiQX/O" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 </head>
 <body>
 <div class="container" id="container1">
     <div class="jumbotron">
         <h1>Count Inventory</h1>
         <p>Select model to add to inventory. DO NOT REFRESH AFTER ADDING.</p>
-        <button class="btn btn-outline-primary w-100" onclick="location='view.php'" ">VIEW ADDED MODELS</button>
+        <button class="btn btn-outline-primary w-100" onclick="location='view.php'"><i class="fas fa-th-list"></i> VIEW ADDED MODELS</button>
     </div>
     <div class="input-field">
         <form id="calculator" method="post" action="">
@@ -67,7 +67,7 @@
                 <input id="qty" name="qty" class="form-control" placeholder="Qty" type="number" />
             </div>
             <div class="btn-group d-flex" role="group">
-                <button class="btn btn-primary w-100" id="generate" type="submit">ADD</button>
+                <button class="btn btn-primary w-100" id="generate" type="submit"><i class="fas fa-plus"></i> ADD</button>
             </div>
         </form>
     </div>
