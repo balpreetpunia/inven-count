9382b9af76a484eac888dde03b239be708737a39
balpreetpunia <mebalpreetpunia@gmail.com>
2018-09-20 15:07:51 -0400

    Bug fix, ra.php

--- ra.php
+++ ra.php
@@ -7,27 +7,30 @@ $model = strtoupper($model);
 $qty = !empty($_POST['qty']) ? $_POST['qty'] : 1;
 $error = -1;
 $time = date("Y-m-d H:i:s");
-
+$postQty = 0;
 
 require "shared/connect.php";
 
 if ($model != ''){
 
-    $sql = "select model from inventory_ra where model = '$model'";
+    $sql = "select model, qty_in_hand, counted from inventory_ra where model = '$model'";
     $sth = $dbh->prepare($sql);
     $sth->execute();
+    $available = $sth->fetchAll();
     $count = $sth->rowCount();
 
     if($count > 0){
         if($qty == '') {
-            $sql = "UPDATE inventory_ra SET counted = counted +1 , Last_Updated = '$time' WHERE model = '$model'";
+            $sql = "UPDATE inventory_ra SET counted = counted +1 WHERE model = '$model'";
             $dbh->exec($sql);
             $error = 0;
+            $postQty = 1;
         }
         else{
-            $sql = "UPDATE inventory_ra SET counted = counted +$qty , Last_Updated = '$time' WHERE model = '$model'";
+            $sql = "UPDATE inventory_ra SET counted = counted +$qty WHERE model = '$model'";
             $dbh->exec($sql);
             $error = 0;
+            $postQty = $qty;
         }
     }
     else{
@@ -46,13 +49,14 @@ $dbh=null;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-aNUYGqSUL9wG/vP7+cWZ5QOM4gsQou3sBfWRr/8S3R1Lv0rysEmnwsRKMbhiQX/O" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 </head>
 <body>
 <div class="container" id="container1">
     <div class="jumbotron">
         <h1>Count Inventory</h1>
         <p>Select model to add to inventory. DO NOT REFRESH AFTER ADDING.</p>
+        <button class="btn btn-outline-primary w-100" onclick="location='view.php'"><i class="fas fa-th-list"></i> VIEW ADDED MODELS</button>
     </div>
     <div class="input-field">
         <form id="calculator" method="post" action="">
@@ -63,13 +67,14 @@ $dbh=null;
                 <input id="qty" name="qty" class="form-control" placeholder="Qty" type="number" />
             </div>
             <div class="btn-group d-flex" role="group">
-                <button class="btn btn-primary w-100" id="generate" type="submit">Add</button>
+                <button class="btn btn-primary w-100" id="generate" type="submit"><i class="fas fa-plus"></i> ADD</button>
             </div>
         </form>
     </div>
     <hr>
     <?php
-    if ($error==0){echo"<h3 class='text-center'>$model Added</h3>";}
+    if ($error==0){
+        echo"<h3 class='text-center'>$model Added</h3><br><h6 class='text-center'>In Hand: ".$available[0]['qty_in_hand']." Counted: ".($available[0]['counted']+($postQty))."</h6>";}
     elseif($error ==1){
         echo "<h3 class='text-center'>Model not found <a href='add_ra.php?model=$model&qty=$qty'>Add $model to Database Qty : $qty</a></h3>";
     }
