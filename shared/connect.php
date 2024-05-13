67b3f9151087239fb0154ab2b6c50daa485e0a13
Balpreet Punia (surfy) <mebalpreetpunia@gmail.com>
2024-05-14 00:28:56 +0530

    Update connect.php

--- shared/connect.php
+++ shared/connect.php
@@ -2,7 +2,7 @@
     try{
 
 
-        $dbh = new PDO("mysql:host=den1.mysql5.gear.host;dbname=teletimeinven","teletimeinven","Sonypsp26");
+        $dbh = new PDO("mysql:host=den1.mysql5.gear.host;dbname=teletimeinven","teletimeinven","Rexdale@407");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e){
     echo 'Database Connection failed: ' . $e->getMessage();
